0e695cc132ba45c24e6ba69131ec92ec569cfbfb
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-02-03 18:09:00 +0100

    form modal and nav added

---
+++ js/app.js
@@ -0,0 +1,9 @@
+document.getElementById('button').addEventListener('click',
+function() {
+  document.querySelector('.bg-modal').style.display = 'flex'
+})
+
+document.querySelector('.close').addEventListener('click', 
+function() {
+  document.querySelector('.bg-modal').style.display = 'none'
+})
