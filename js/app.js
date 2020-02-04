1125e7ba853f91ee3ceb15496026ea3042e993b3
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-02-04 16:18:48 +0100

    table to read the books added

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 document.getElementById('button').addEventListener('click',
-function() {
-  document.querySelector('.bg-modal').style.display = 'flex'
-})
+  () => {
+    document.querySelector('.bg-modal').style.display = 'flex';
+  });
 
-document.querySelector('.close').addEventListener('click', 
-function() {
-  document.querySelector('.bg-modal').style.display = 'none'
-})
+document.querySelector('.close').addEventListener('click',
+  () => {
+    document.querySelector('.bg-modal').style.display = 'none';
+  });
---
+++ js/book.js
@@ -0,0 +1,21 @@
+const myLibrary = [];
+
+function Book(title, page, author, year, genre, status) {
+  // the constructor
+  this.title = title;
+  this.page = page;
+  this.author = author;
+  this.year = year;
+  this.genre = genre;
+  this.status = status;
+}
+
+function addBookToLibrary = (el) => {
+  el.preventDefault();
+
+  const titleVal =  
+}
+// eslint-disable-next-line no-console
+console.log(myLibrary);
+Book();
+addBookToLibrary();
