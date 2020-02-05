80c5b23c533c31dd23035aba7bebee268c9e5c7b
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-02-05 17:12:38 +0100

    added logic

--- js/book.js
+++ js/book.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-alert */
 const myLibrary = [];
 
 function Book(title, page, author, year, genre, status) {
@@ -10,12 +11,45 @@ function Book(title, page, author, year, genre, status) {
   this.status = status;
 }
 
-function addBookToLibrary = (el) => {
-  el.preventDefault();
-
-  const titleVal =  
+function updatelocalStorage() {
+  window.localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 }
+
+function deleteItem() {
+  // splice and remove one item selected
+  updatelocalStorage(myLibrary);
+  // re-render and update the page with the new set of data
+}
+
+function pushBook(book) {
+  myLibrary.push(book);
+  window.localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+  // re-render and update the page with the new set of data
+}
+
+function addBookToLibrary() {
+  const titleVal = document.getElementById('title').value;
+  const pageVal = document.getElementById('pageNumber').value;
+  const authorVal = document.getElementById('authorName').value;
+  const yearVal = document.getElementById('yearPublished').value;
+  const genreVal = document.getElementById('genre').value;
+  const statusVal = document.getElementById('status').value;
+
+  if (titleVal === '' || pageVal === '' || authorVal === '' || yearVal === '') {
+    return alert("fields can't be blank!");
+  }
+
+  const newBook = new Book(titleVal, pageVal, authorVal, yearVal, genreVal, statusVal);
+
+  return pushBook(newBook);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('add').addEventListener('click', addBookToLibrary);
+});
+
 // eslint-disable-next-line no-console
 console.log(myLibrary);
 Book();
 addBookToLibrary();
+deleteItem();
