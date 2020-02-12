91993386e16d4c5bfe82f1d07daa6c442ac88df8
collins <ugwucollins64@gmail.com>
2020-02-12 10:57:26 +0100

    restructuring the app to accomodate eslint

--- js/book.js
+++ js/book.js
@@ -131,4 +131,40 @@ function render() {
     editBook(index);
   });
 }
+
+function deleteBook(index) {
+  document.querySelector(`.delete-${index}`).addEventListener('click', function deleteButton() {
+    const bookId = this.getAttribute('data-id');
+    document.querySelector(`.book-${bookId}`).style.display = 'none';
+    deleteItem(bookId);
+  });
+}
+
+const populateEditForm = (book) => {
+  document.getElementById('title').value = book.title;
+  document.getElementById('pageNumber').value = book.page;
+  document.getElementById('authorName').value = book.author;
+  document.getElementById('yearPublished').value = book.year;
+  document.getElementById('genre').value = book.genre;
+  document.getElementById('status').value = book.status;
+  document.querySelector('.bg-modal').style.display = 'flex';
+};
+
+function editBook(index) {
+  document.querySelector(`.edit-${index}`).addEventListener('click', function editButton() {
+    const bookId = this.getAttribute('data-id');
+    const query = document.querySelector('.general-button');
+    query.removeEventListener('click', addBookToLibrary);
+    query.removeAttribute('id');
+    query.setAttribute('id', 'edit');
+    query.innerHTML = 'Update';
+    const book = getABook(bookId);
+    populateEditForm(book);
+
+    document.getElementById('edit').addEventListener('click', () => {
+      updateBook(index);
+    });
+  });
+}
+
 render();
